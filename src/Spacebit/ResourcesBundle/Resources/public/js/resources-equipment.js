a37e8c74f6285e2f3805d0aaccac82361b235578
Achini <achini.nh@gmail.com>
2016-01-07 22:22:35 +0530

    load euipment by catagory upon selection

---
+++ src/Spacebit/ResourcesBundle/Resources/public/js/resources-equipment.js
@@ -0,0 +1,86 @@
+/**
+ * Created by Achini on 07/01/2016.
+ */
+
+function loadDepartments() {
+    var faculty = document.getElementById('faculty').value; //get the value selected via id.value & assign to variable
+
+    var obj;
+
+    if (window.XMLHttpRequest) {
+        obj = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        obj = new ActiveXObject("Microsoft.XMLHTTP");
+    } else {
+        alert("Browser Doesn't Support AJAX!");
+    }
+
+    if (obj !== null) {
+        obj.onreadystatechange = function () {
+            if (obj.readyState < 4) {
+                // progress
+            } else if (obj.readyState === 4) {
+                var res = obj.responseText;
+                var depts = JSON.parse(res).depts;
+
+                var content = '<option disabled selected>*Please select a department*</option>;';
+                for (var i = 0; i < depts.length; i++) {
+                    content += '<option value="' + depts[i].dept_name + '">' + depts[i].dept_name + '</option>';
+                }
+                document.getElementById('department').innerHTML = content;
+
+                document.getElementById('department').disabled = false;
+            }
+        }
+
+        obj.open("POST", "./equipment/getDepartments", true);
+        obj.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        obj.send("faculty=" + faculty); //pass faulty to controller
+    }
+}
+
+function loadEquipmentByCatagory() {
+    var department = document.getElementById('department').value;
+
+    var obj;
+
+    if (window.XMLHttpRequest) {
+        obj = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        obj = new ActiveXObject("Microsoft.XMLHTTP");
+    } else {
+        alert("Browser Doesn't Support AJAX!");
+    }
+
+    if (obj !== null) {
+        obj.onreadystatechange = function () {
+            if (obj.readyState < 4) {
+                // progress
+            } else if (obj.readyState === 4) {
+                var res = obj.responseText;
+                var deptEquipments = JSON.parse(res).deptEquipment;
+
+                alert(res);
+
+                var tableContent = '';
+
+                tableContent = '<h2 style="text-align: center;">' + '</h2><table class="table table-hover">';
+                tableContent += '<tr><th>Equipment Catogary</th><th>Equipment Request</th></tr>';
+                for (var i = 0; i < deptEquipments.length; i++) {
+                    tableContent += '<tr>';
+                    tableContent += '<td>' + deptEquipments[i].equipment_type + '</td>';
+                    tableContent += '<td>' + '"REQUEST"' + '</td>';
+                    tableContent += '</tr>';
+                }
+                tableContent += '</table>';
+
+                document.getElementById('equipmentTableContent').innerHTML = tableContent;
+                $('#equipmentArea').modal();
+            }
+        }
+    }
+
+    obj.open("POST", "./equipment/getByCategory", true);
+    obj.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    obj.send("department=" + department);
+}
