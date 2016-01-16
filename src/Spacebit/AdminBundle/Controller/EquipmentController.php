96cce53c91e4d8ac5d170a9e22f564d14bb6c4b6
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2016-01-17 00:16:47 +0530

    Finalized

--- src/Spacebit/AdminBundle/Controller/EquipmentController.php
+++ src/Spacebit/AdminBundle/Controller/EquipmentController.php
@@ -10,14 +10,26 @@ class EquipmentController extends Controller
 {
     public function equipmentAction()
     {
+<<<<<<< HEAD
+
+        $this->get('session')->set('user_id', 'CS1423');
+=======
     
         $this->get('session')->set('user_id', 'AB1234');
         $this->get('session')->set('first_name', 'John');
         $this->get('session')->set('last_name', 'Doe');
         $this->get('session')->set('access_level', 5);
 
+>>>>>>> origin/master
         $conn = $this->get('database_connection');
-        $stmt = $conn->prepare('SELECT request_id, user_id,equipment.resource_id, date_from,date_to, time_from,time_to, status FROM resource_request INNER JOIN equipment on  equipment.resource_id = resource_request.resource_id ORDER BY status DESC, date_from DESC, time_from DESC;');
+
+        $stmt = $conn->prepare("SELECT dept_name FROM staff  where user_id =:user_id;");
+        $stmt->bindValue(':user_id', $this->get('session')->get('user_id'));
+        $stmt->execute();
+        $staff_member = $stmt->fetch();
+
+        $stmt = $conn->prepare('SELECT request_id,type, user_id,resource_request.resource_id,date_from,date_to, time_from,time_to, status,type,department_name FROM resource_request  where (resource_request.resource_id is NULL  or resource_request.resource_id  in (select resource_id from equipment))  and resource_request.department_name = :dept_name ORDER BY status DESC, date_from DESC, time_from DESC;');
+        $stmt->bindValue(':dept_name', $staff_member['dept_name']);
         $stmt->execute();
         $equipment_requests = $stmt->fetchAll();
 
@@ -55,6 +67,26 @@ class EquipmentController extends Controller
 
         return $response;
     }
+    public function getByResourceTypeAction()
+    {
+       $request = Request::createFromGlobals();
+        $department_name = $request->request->get('department_name');
+        $type = $request->request->get('type');
+
+
+        $conn = $this->get('database_connection');
+        $stmt = $conn->prepare('SELECT resource_id FROM equipment INNER JOIN resource USING(resource_id) WHERE department_name = :department_name AND equipment_type= :type ;');
+        $stmt->bindValue(':type', $type);
+        $stmt->bindValue(':department_name', $department_name);
+
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+
+        $response = new Response(json_encode(array('result' => $result)));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
 
     public function addEditAction()
     {
@@ -63,8 +95,13 @@ class EquipmentController extends Controller
         $equipment_type = $request->request->get('equipment_type');
         $description = $request->request->get('description');
         $availability = $request->request->get('availability');
+
+        $availability = ((string)$availability=='true')?1:0;
+
         $value = $request->request->get('value');
-        $update_type = $request->request->get('update_type');
+        $update_type = $request->request->get('update-type');
+        $department_name = $request->request->get('department_name');
+
 
         $conn = $this->get('database_connection');
 
@@ -89,18 +126,22 @@ class EquipmentController extends Controller
 
             }
 
-            $stmt = $conn->prepare('INSERT into equipment values(:resource_id, :value, :equipment_type);');
+            $stmt = $conn->prepare('INSERT into equipment values(:resource_id, :value, :equipment_type, :department_name);');
             $stmt->bindValue(':resource_id', $resource_id);
             $stmt->bindValue(':value', $value);
             $stmt->bindValue(':equipment_type', $equipment_type);
+            $stmt->bindValue(':department_name', $department_name);
+
 
             $response = 'success';
             if(!$stmt->execute()) {
                 $response = $stmt->errorCode();
             }
+            $response->headers->set('Content-Type', 'application/json');
+            return new Response($response);
         } else {
             $stmt = $conn->prepare('UPDATE resource SET description = :description, availability = :availability WHERE resource_id = :resource_id;');
-            $stmt->bindValue(':resoucre_id', $resource_id);
+            $stmt->bindValue(':resource_id', $resource_id);
             $stmt->bindValue(':availability', $availability);
             $stmt->bindValue(':description', $description);
 
@@ -118,9 +159,11 @@ class EquipmentController extends Controller
                     }
                 }
             }
-            $stmt = $conn->prepare('UPDATE equipment SET value = :value, equipment_type = :equipment_type WHERE resource_id = :resource_id;');
+            $stmt = $conn->prepare('UPDATE equipment SET value = :value, equipment_type = :equipment_type , department_name = :department_name WHERE resource_id = :resource_id;');
             $stmt->bindValue(':resource_id', $resource_id);
             $stmt->bindValue(':value', $value);
+            $stmt->bindValue(':department_name', $department_name);
+
             $stmt->bindValue(':equipment_type', $equipment_type);
 
             $response = 'success';
@@ -137,8 +180,9 @@ class EquipmentController extends Controller
     function changeRequestStatusAction()
     {
         $request = Request::createFromGlobals();
-        $request_id = $request->request->get('request-id');
+        $request_id = $request->request->get('request_id');
         $status = $request->request->get('status');
+        $resource_id= $request->request->get('resource_id');
 
         $conn = $this->get('database_connection');
         $conn->beginTransaction();
@@ -147,9 +191,11 @@ class EquipmentController extends Controller
         try {
 
 
-            $stmt = $conn->prepare('UPDATE resource_request SET status = :status,WHERE request_id = :request_id;');
+            $stmt = $conn->prepare('UPDATE resource_request SET status = :status , resource_id = :resource_id WHERE request_id = :request_id;');
             $stmt->bindValue(':status', $status);
             $stmt->bindValue(':request_id', $request_id);
+            $stmt->bindValue(':resource_id', $resource_id);
+
             if (!$stmt->execute()) {
                 throw new \Symfony\Component\Config\Definition\Exception\Exception();
             }
--- src/Spacebit/AdminBundle/Resources/public/js/admin-venues.js
+++ src/Spacebit/AdminBundle/Resources/public/js/admin-venues.js
@@ -82,15 +82,16 @@ function showEditVenueModal(resource_id) {
                 var res = obj.responseText;
                 var venue = JSON.parse(res).result;
 
+
                 document.forms['venue-add-form']['resource_id'].value = venue.resource_id;
                 document.forms['venue-add-form']['description'].value = venue.description;
                 document.forms['venue-add-form']['capacity'].value = venue.capacity;
                 document.forms['venue-add-form']['closing_time'].value = venue.closing_time;
                 document.forms['venue-add-form']['dept_name'].value = venue.dept_name;
-                document.forms['venue-add-form']['name'].value = vanue.name;
+                document.forms['venue-add-form']['name'].value = venue.name;
                 document.forms['venue-add-form']['opening_time'].value = venue.opening_time;
                 document.forms['venue-add-form']['venue_type'].value = venue.venue_type;
-                document.forms['venue-add-form']['availability'].checked =venue.availability;
+                document.forms['venue-add-form']['availability'].checked =(venue.availability.toString()=='1')?true:false;
                 document.forms['venue-add-form']['submit-button'].value = 'Edit';
 
 
@@ -100,7 +101,7 @@ function showEditVenueModal(resource_id) {
 
                 document.forms['venue-add-form']['submit-button'].innerHTML = '<span class="glyphicon glyphicon-ok"></span> OK';
                 $('#manage-venues-modal').modal('hide');
-                $('#add-edit-venue-modal').modal();
+                setTimeout("$('#add-edit-venue-modal').modal();", 1000);
             }
         }
 
@@ -109,19 +110,17 @@ function showEditVenueModal(resource_id) {
         obj.send('resource_id=' + resource_id);
     }
 }
-function changeRequest(requestId) {
+function changeRequest(requestID) {
     //can't find the bug
-    modalContent = '<button value="0" id="DeclineButton" class="btn btn-xs btn-info" onclick="EditRequest(requestID,0)"><span class="glyphicon glyphicon-pencil"></span> Decline</button> ';
-    modalContent += '<button value="1" id="ApproveButton" class="btn btn-xs btn-info" onclick="EditRequest(requestID,1)"><span class="glyphicon glyphicon-pencil"></span> Approve</button> ';
-    modalContent += '<button value="2" id="ApproveButton" class="btn btn-xs btn-info" onclick="EditRequest(requestID,2)"><span class="glyphicon glyphicon-pencil"></span> Pendin</button> ';
+    modalContent = '<button value="1" id="ApproveButton" class="btn btn-xs btn-success" onclick="EditRequest(' + requestID + ', 1)"><span class="glyphicon glyphicon-ok"></span> Approve</button> ';
+    modalContent += '<button value="0" id="DeclineButton" class="btn btn-xs btn-danger" onclick="EditRequest(' + requestID + ',0)"><span class="glyphicon glyphicon-remove"></span> Decline</button> ';
+    modalContent += '<button value="2" id="ApproveButton" class="btn btn-xs btn-primary" onclick="EditRequest(' + requestID + ',2)"><span class="glyphicon glyphicon-time"></span> Pending</button> ';
 
 
     document.getElementById('edit-request-div').innerHTML = modalContent;
     $('#edit-venues-modal').modal();
 }
 function EditRequest(request_id,status) {
-    alert("fuck");
-    alert("Fuck");
 
     var obj;
 
@@ -148,12 +147,12 @@ function EditRequest(request_id,status) {
                 modalContent += ' Ok</button><div></div>';
                 document.getElementById('message-modal-content').innerHTML = modalContent;
 
-                $('#edit-equipments-modal').modal('hide');
+                $('#edit-venues-modal').modal('hide');
                 $('#message-modal').modal();
-            }
+                location.reload();           }
         }
 
-        obj.open("POST", "./vehicle/ changeRequestStatus", true);
+        obj.open("POST", "./venues/changeRequestStatus", true);
         obj.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         obj.send("request_id=" + request_id + '&status=' + status);
     }
@@ -164,13 +163,13 @@ function EditRequest(request_id,status) {
 
     var resource_id = document.forms['venue-add-form']['resource_id'].value;
     var description= document.forms['venue-add-form']['description'].value;
-    var availability = document.forms['venue-add-form']['availability'].value;
-    var availability = document.forms['venue-add-form']['capacity'].value;
-    var availability = document.forms['venue-add-form']['closing_time'].value;
-    var availability = document.forms['venue-add-form']['dept_name'].value;
-    var availability = document.forms['venue-add-form']['name'].value;
-    var availability = document.forms['venue-add-form']['opening_time'].value;
-    var availability = document.forms['venue-add-form']['venue_type'].value;
+    var availability = document.forms['venue-add-form']['availability'].checked;
+    var capacity = document.forms['venue-add-form']['capacity'].value;
+    var closing_time= document.forms['venue-add-form']['closing_time'].value;
+    var dept_name = document.forms['venue-add-form']['dept_name'].value;
+    var name = document.forms['venue-add-form']['name'].value;
+    var opening_time = document.forms['venue-add-form']['opening_time'].value;
+    var venue_type = document.forms['venue-add-form']['venue_type'].value;
 
     var updateType = document.forms['venue-add-form']['submit-button'].value;
     var obj;
@@ -189,11 +188,12 @@ function EditRequest(request_id,status) {
             } else if (obj.readyState === 4) {
                 var res = obj.responseText;
 
+
                 var modalContent = '<div style="margin: 10px;"><p>';
                 if (res == 'success') {
-                    modalContent += 'Venue with resource id ' + resource_id + ' was ' + (updateType == "Add" ? "added" : "edited") + ' successfully</p><button class="btn btn-sm btn-success" onclick=\'$("#message-modal").modal("hide"); showManageVenuesModal();\'><span class="glyphicon glyphicon-ok"></span>';
+                    modalContent += 'Venue with resource id ' + resource_id + ' was ' + (updateType == "Add" ? "added" : "edited") + ' successfully</p><button class="btn btn-sm btn-success" onclick=\'$("#message-modal").modal("hide"); showManageVenueModal();\'><span class="glyphicon glyphicon-ok"></span>';
                 } else {
-                    modalContent += 'An error occurred in ' + (updateType == "Add" ? "adding" : "editing") + ' the venue with Resource ID ' + resource_id + '. Sorry for the inconvenience.</p><div style="text-align: center;"><button class="btn btn-sm btn-danger" onclick=\'$("#message-modal").modal("hide"); showManageVenuesModal();\'><span class="glyphicon glyphicon-warning-sign"></span>';
+                    modalContent += 'An error occurred in ' + (updateType == "Add" ? "adding" : "editing") + ' the venue with Resource ID ' + resource_id + '. Sorry for the inconvenience.</p><div style="text-align: center;"><button class="btn btn-sm btn-danger" onclick=\'$("#message-modal").modal("hide"); showManageVenueModal();\'><span class="glyphicon glyphicon-warning-sign"></span>';
                 }
                 modalContent += ' Ok</button><div></div>';
                 document.getElementById('message-modal-content').innerHTML = modalContent;
@@ -203,7 +203,7 @@ function EditRequest(request_id,status) {
             }
         }
 
-        obj.open("POST", "./venue/addEdit", true);
+        obj.open("POST", "./venues/addEdit", true);
         obj.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         obj.send("resource_id=" + resource_id+   '&availability=' + availability + '&description=' + description + '&capacity=' + capacity +  '&closing_time=' + closing_time + '&dept_name=' + dept_name + '&name=' + name +'&opening_time=' + opening_time +'&venue_type=' + venue_type + '&update-type=' + updateType);
     }
